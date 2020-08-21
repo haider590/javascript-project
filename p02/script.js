01b4f95908566eb3993141f128f64942ba6d76d8
haider590 <69319906+haider590@users.noreply.github.com>
2020-08-21 21:34:06 +0500

    updated javascript
    new update to screen aplied as now it shows the trailer of the selected movie.

--- p02/script.js
+++ p02/script.js
@@ -3,6 +3,7 @@ const seats = document.querySelectorAll('.row.seat:not(.occupied)');
 const count = document.getElementById('count');
 const total = document.getElementById('total');
 const movieSelect = document.getElementById('movie');
+const screen = document.querySelector('.screen');
 let ticketPrice = +movieSelect.value;
 
 populateUI()
@@ -65,3 +66,28 @@ container.addEventListener('click', (e) => {
 
 //calculate initial number of seats and total price
 updateSelectedCount();
+
+
+
+// for movie trailer
+const trailerLink = ['https://www.youtube.com/embed/alQlJDRnQkE','https://www.youtube.com/embed/ICE1tPVFkhA','https://www.youtube.com/embed/CI4EjV_x_PQ','https://www.youtube.com/embed/OuXkXJ1fPuk','https://www.youtube.com/embed/t86sKsR4pnk'];
+
+
+//function to play movie trailer 
+
+function playTrailer(index) {
+    screen.innerHTML = `<iframe width='350' height='800' src='${trailerLink[index]}?autoplay=1&loop=1&controls=0' frameborder='3' allow='accelerometer; autoplay; encrypted-media; gyroscope; picture-in-picture'></iframe>`;
+}
+
+
+// event listener for changing the trailer as movie is changed
+movie.addEventListener('change',(event)=>{
+    localStorage.setItem('movieIndex',event.target.selectedIndex);
+    playTrailer( localStorage.getItem('movieIndex') );
+    localStorage.setItem('movieIndex',JSON.parse(movie.selectedIndex));
+    displayResult(JSON.stringify(selectedSeats.length),JSON.stringify(selectedSeats.length * movie[localStorage.getItem('movieIndex')].value));
+});
+
+
+// calling function to Load trailer
+playTrailer((localStorage.getItem('movieIndex') != null)? localStorage.getItem('movieIndex') : 0);
